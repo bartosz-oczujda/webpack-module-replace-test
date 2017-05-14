9762b9189396b2babcf51ef62b3fab6a47d4374b
Bartosz Oczujda <bartosz.oczujda@gmail.com>
2017-05-14 12:49:32 +0200

    Add Assertions for inheritance chain tests, improve comments

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import { TesterSubclass } from "./TesterSubclass";
 import { Tester as TestReplacer} from "../src_repl/Tester_repl"
 
 //I want to replace this class with TestReplacer
-//This is what NormalModuleReplacemntPlugin is doing in the webpack config
+//This is what NormalModuleReplacementPlugin is doing in the webpack config
 let test = new Tester();
 let testSubclass = new TesterSubclass();
 
@@ -16,13 +16,17 @@ chai.assert.equal(test.getNum(), 55, "getNum returns value from replaced supercl
 chai.assert.equal(testSubclass.getNum(), 33, "getNum returns proper value from subclass");
 chai.assert.equal((testSubclass as TestReplacer).getStr(), "Foo", "it's possible to add and use new methods");
 
+console.log("Simple class replacement tests passed");
 
 //Testing replacements in a longer inheritance chain
 import { ClassFive } from "./inheritance_chain/ClassFive";
 import { ClassThree } from "../src_repl/inheritance_chain/ClassThree";
 
 let five = new ClassFive();
-console.log(five.getFullName());
+chai.assert.equal(five.getFullName(), "ReplacedClassFive", "webpack succesfully replaced ClassThree");
 
 //limitation - casting does not work in this case so we need to cast as any
-console.log((five as any).inheritedFunction())
+//but I dont know if this will ever be necessary
+chai.assert.isFunction((five as any).inheritedFunction, "it is possible to use new functions from replaced classes in the inheritance chain");
+
+console.log("Inheritance chain replacement tests passed");
